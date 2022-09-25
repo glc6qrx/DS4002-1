61d325bedc0dc524369ab1ed2e041fe16680c7c3
catherineschuster <85313242+catherineschuster@users.noreply.github.com>
2022-09-25 10:57:46 -0400

    Update AgeDetectionModel.py

--- src/AgeDetectionModel.py
+++ src/AgeDetectionModel.py
@@ -23,7 +23,7 @@ from zipfile import ZipFile
 #################################################
 # Loop through age file names to extract ages
 #################################################
-path = "~/DS4002-1/UTKFace.tar"
+path = "~/DS4002-1/src/UTKFace.tar"
 
 
 with ZipFile('UTKFace.tar', 'r') as zipObj:
