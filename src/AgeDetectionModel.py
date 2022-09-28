b8649d8ed2d4c57ea22212c966dcd0757a5b8eeb
catherineschuster <85313242+catherineschuster@users.noreply.github.com>
2022-09-28 10:14:41 -0400

    Update AgeDetectionModel.py

--- src/AgeDetectionModel.py
+++ src/AgeDetectionModel.py
@@ -158,8 +158,9 @@ age_nba = []
 for img in os.listdir(path):
   ages = img.split("_")[0]
   img = cv2.imread(str(path)+"/"+str(img))
-  img = img.reshape(1,200,200,3)
   img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+  img = cv2.resize(img, (200, 200))
+  img = np.resize(img, (200, 200, 3))
   img = img / 255
   images_nba.append(np.array(img))
   age_nba.append(np.array(ages))
